88860c97834769997bfeb951d9a8b9f70d21df8c
KoheiNegishi <nk.0910.shanti@gmail.com>
2024-06-19 17:20:24 +0900

    fix:ルーティングの変更、テストファイルの作成

---
+++ src/tests/Feature/HelloTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HelloTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+
+        $response = $this->get('/');
+        $response->assertStatus(200);
+
+
+    }
+}
